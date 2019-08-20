e71421dd866abf97a1d19d43fe95c768ad371458
Hussain_Zaidi <hussain.haider@carteblanche.tech>
2019-08-20 22:18:52 +0500

    Add styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
+import './Person.css'
 
 const Person = (props) => {
     // {} use to output the dynamic data.
     // Also in class component we have to use this.props to access the attributes
     return (
-        <div>
+        <div className="Person">
             <p>I'm a { props.name } and I am { props.age } years old.!!! </p>
             {/*to get the text/output between the opening and closing tag of our custom component*/}
             {/*react give us a simple props property named children*/}
