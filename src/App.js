fd8b46ccbb120d4bf2ba9186185098e63d41d9b6
Hussain_Zaidi <hussain.haider@carteblanche.tech>
2019-08-20 17:53:38 +0500

    State property added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Person />
-      </div>
-    );
-  }
+    // state property is inside the component and
+    // we can only access it when a class extends from Component class.
+    // We cannot do this in the functional component. State is the data of the component.
+    // State can change but if it changes it leads  React to re-render the DOM or update the DOM.
+    state = {
+      person :[
+          {name:'Max',age:28},
+          {name:'Manu',age:27}
+      ]
+    };
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                {/* we can take these attributes and get inside the receiving*/}
+                {/*component on object named props.*/}
+                <Person name={ this.state.person[0].name } age={ this.state.person[0].age }/>
+                <Person name={ this.state.person[1].name } age={ this.state.person[1].age } />
+            </div>
+        );
+    }
 }
 
 export default App;
